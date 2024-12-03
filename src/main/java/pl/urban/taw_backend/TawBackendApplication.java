4629434863ef12e363b9fcf138bce512b57be4d4
Gosqu248 <grzegorzurban248@gmail.com>
2024-12-03 12:52:46 +0100

    first commit

---
+++ src/main/java/pl/urban/taw_backend/TawBackendApplication.java
@@ -0,0 +1,13 @@
+package pl.urban.taw_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TawBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TawBackendApplication.class, args);
+    }
+
+}
